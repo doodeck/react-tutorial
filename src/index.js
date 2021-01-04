ff128678d0d763208808ba242aa552a8add6689d
doodeck <8515450+doodeck@users.noreply.github.com>
2021-01-04 18:00:25 +0100

    Experimenting with a new class

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ShoppingList from './ShoppingList';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ShoppingList name="Mark" />
   </React.StrictMode>,
   document.getElementById('root')
 );
